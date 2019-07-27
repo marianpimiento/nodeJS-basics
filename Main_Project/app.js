d896818f79044d15656cd6109aac9090c0059143
mariapaladin <maria@paladin.insure>
2019-07-26 19:35:30 -0500

    Send response inside middleware and use app.listen

--- Main_Project/app.js
+++ Main_Project/app.js
@@ -1,18 +1,15 @@
-const http = require('http');
 const express = require('express');
 
 const app = express();
 
 app.use((req, res, next) => {
   console.log('In the middleware');
-  next();
+  next(); // Allows the request to continue to the next middleware in line
 });
 
 app.use((req, res, next) => {
   console.log('In another middleware');
-  // Send response
+  res.send('<h1>Hello from Express</h1>'); // Send response
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
